d6b53713ce034329d5b7d41503fad8a0115e229a
Yuval Birenboum <bir.yuval@gmail.com>
2016-11-16 14:43:57 +0200

    ui changes, added heap statistics

--- example/index.js
+++ example/index.js
@@ -3,3 +3,24 @@ const visualV8 = require('../index.js')({
     port: 1919,
     interval: 3000
 });
+
+/* 
+this causes memory leaks
+*/
+/*
+var theThing = null;
+var replaceThing = function () {
+  var originalThing = theThing;
+  var unused = function () {
+    if (originalThing)
+      console.log("hi");
+  };
+  theThing = {
+    longStr: new Array(1000000).join('*'),
+    someMethod: function () {
+      console.log(someMessage);
+    }
+  };
+};
+setInterval(replaceThing, 1000);
+*/
--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 var instance = null,
     usageInterval = null,
-    profilerMemoryDiffInterval = null,
     cpuTimout = null;
 
 const defaultConfig = {
@@ -17,6 +16,8 @@ const VisualV8 = function(config){
 }
 
 function Run(config){
+    const v8 = require('v8');
+    const os  = require('os-utils');
     var profiler = require('v8-profiler');
     var fs = require('fs');
     var server = require('http').createServer(handleRequest.bind(this));
@@ -39,7 +40,6 @@ function Run(config){
 
     io.on('end', function (){
         clearInterval(usageInterval);
-        clearInterval(profilerMemoryDiffInterval);
     });
 
     io.on('connection', function(socket) {  
@@ -51,28 +51,22 @@ function Run(config){
             cpuSnapshot(config.interval, (snapshot)=>socket.emit('cpu', { message: snapshot }));            
         }, config.interval);
 
-        var snapshot1 = profiler.takeSnapshot()
-            snapshot2 = null, 
-            diff = null;
-        socket.on('snapshot-diff', function(message) {
-            snapshot2 = profiler.takeSnapshot();
-            diff = snapshot1.compare(snapshot2);
-            snapshot1 = snapshot2;
-            snapshot2.delete();
-            snapshot2 = null;
-            socket.emit('snapshot-diff-result', { message: diff });
-            diff = null;
+        socket.on('heap-snapshot', function(message) {  
+            var snapshot1 = profiler.takeSnapshot();
+            // Export snapshot to file file
+            snapshot1.export(function(error, result) {
+                socket.emit('heap-snapshot-result', { message: result });
+                snapshot1.delete();
+                snapshot1 = null;
+            });
         });
 
-        socket.on('snapshot-to-file', function(message) {  
-        });
-
-        socket.on('gc', function(message) { 
+        socket.on('gc', function(message) {
             if (global.gc) {
                 global.gc();
                 socket.emit('gc-result', { message: 'GC collect completed' });
             } else {
-                socket.emit('gc-result', { message: 'Garbage collection unavailable.  Pass --expose-gc when launching node to enable forced garbage collection.' });
+                socket.emit('gc-result', { message: 'Garbage collection unavailable. Pass --expose-gc when launching node to enable forced garbage collection.' });
             }
         });
         
@@ -81,80 +75,18 @@ function Run(config){
     function memorySnapshot(){
         var snapshot = process.memoryUsage();
         snapshot.time = new Date().getTime();
+        snapshot.heapSpaceStatistics = v8.getHeapSpaceStatistics();
         return snapshot;
     }
 
     function cpuSnapshot(interval, cb){
-        if (interval <= 1500)
-            interval = interval/2;
-        else
-            interval = 1000;
-
-        const os = require('os');
-        const NUMBER_OF_CPUS = os.cpus().length;
-        var startTime  = process.hrtime()
-        var startUsage = process.cpuUsage()
-
-        clearTimeout(cpuTimout);
-        cpuTimout = setTimeout(() => {
-            // spin the CPU for 500 milliseconds
-            var now = Date.now()
-            while (Date.now() - now < 500);
-
-            const newTime = process.hrtime();
-            const newUsage = process.cpuUsage();
-            const elapTime = process.hrtime(startTime)
-            const elapUsage = process.cpuUsage(startUsage)
-            startUsage = newUsage;
-            startTime = newTime;
-
-
-            const elapTimeMS = hrtimeToMS(elapTime)
-
-            const elapUserMS = elapUsage.user / 1000; // microseconds to milliseconds
-            const elapSystMS = elapUsage.system / 1000;
-            const cpuPercent = (100 * (elapUserMS + elapSystMS) / elapTimeMS / NUMBER_OF_CPUS).toFixed(1);
+        os.cpuUsage((vsnapshot)=>{
             var snapshot = {};
-            snapshot.y = parseInt(cpuPercent);
+            snapshot.y = vsnapshot;
             snapshot.x = new Date().getTime();
-            cb(snapshot);                
-        }, interval);
-
-        function hrtimeToMS (hrtime) {
-            return hrtime[0] * 1000 + hrtime[1] / 1000000
-        }
-    }
-
-    /*
-    setInterval(()=>{
-        var snapshot1 = profiler.takeSnapshot();
-        // Export snapshot to file file
-        snapshot1.export(function(error, result) {
-            fs.writeFileSync('snapshot-'+new Date().getMinutes()+'-'+new Date().getSeconds()+'.heapsnapshot', result);
-            snapshot1.delete();
+            cb(snapshot);  
         });
-    }, 5000);
-    */
-
-    
+    };
 }
-
-/*
-var theThing = null;
-var replaceThing = function () {
-  var originalThing = theThing;
-  var unused = function () {
-    if (originalThing)
-      console.log("hi");
-  };
-  theThing = {
-    longStr: new Array(1000000).join('*'),
-    someMethod: function () {
-      console.log(someMessage);
-    }
-  };
-};
-setInterval(replaceThing, 1000);
-*/
 
 module.exports = VisualV8;
